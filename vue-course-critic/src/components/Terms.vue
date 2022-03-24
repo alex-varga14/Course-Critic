7683a785f93a87e8ac3d298ef64fdaf228f350e6
jcacierto <78886859+jcacierto@users.noreply.github.com>
2022-03-24 15:50:12 -0600

    Updated Terms.vue
    Updated and added content to Terms.vue

--- vue-course-critic/src/components/Terms.vue
+++ vue-course-critic/src/components/Terms.vue
@@ -6,14 +6,39 @@
 
         <div class = "terms-list ">
             <div class="row text-center">
-                CourseCode: 
+                CourseCode: Four-letter code representing the faculty name in which a course is being offered
              </div>
             <div class="row text-center">
-                CourseNo:
+                CourseNo: Unique number identifying which class is being offered within the faculty outlined by CourseCode
             </div>
             <div class="row text-center">
-                CourseID:
+                CourseID: Unique ID for each course offering
             </div>
+        </div>
+
+        <div class = "terms-conditions">
+            <p> 
+                These are the terms and conditions that govern the use of this service. All users must accept and comply with
+                the regulations set out. 
+                By using this application, you agree to be bound by the Terms and Conditions on this page.
+                You may not use this service if you refuse to follow these terms and conditions.
+                Contributors, Administrators, and other users of this application may not, under any circumstances, 
+                use this program for commercial purposes. Course Critic is strictly a university course review site, 
+                and using it for commercial purposes is a violation of the terms and conditions. 
+
+                <br><br>
+                The application allows contributors and administrators to post content regarding university courses they have taken.
+                It is your responsibility to provide accurate, reliable, and fair information for other users to access. By posting a review,
+                you understand that: 
+                <ul>
+                    <li> (a) You created the information with no plagiarism involved, and </li>
+                    <li> (b) The review posted for a course does not reveal any personal details about a person, aside from their name and position within the faculty in which they teach. </li> 
+                </ul>
+                 
+                <br>
+
+                Violation of any of these terms within your post is subject to removal by the Administrators. 
+            </p>
         </div>
     </div>
 </template>
@@ -38,8 +63,14 @@ p {
     margin-top: 30px;
 }
 .term-list {
-    margin-left: 20px;
-    margin-right: 20px;
+    margin-left: 200px;
+    margin-right: 200px;
     color: #000000;
+}
+
+.terms-conditions {
+    margin-left: 200px;
+    margin-right: 200px;
+    color: black;
 }
 </style>
